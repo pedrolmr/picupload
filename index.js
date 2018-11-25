4ac81045470f8afdbb70ccacc091c02071722ea3
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-25 02:55:27 -0500

    displaying user posts

--- index.js
+++ index.js
@@ -17,5 +17,23 @@ server.get('/api/users', (req, res) => {
         })
 })
 
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    db('users').where({ id }).first()
+        .then(user => {
+            if (user) {
+                db('posts')
+                    .where({ user_id: id })
+                    .then(posts => {
+                        user.posts = posts;
+                        res.status(200).json(user);
+                    })
+                    .catch(err => res.status(500).json(err))
+            }
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+
 const port = 8000;
 server.listen(port, () => console.log(`running on port: ${port}`))
