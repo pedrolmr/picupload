316875bc969b97f02964bee7527522c682dc6dc2
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-24 23:44:45 -0500

    working on migrations

--- data/migrations/20181124231303_users.js
+++ data/migrations/20181124231303_users.js
@@ -1,6 +1,10 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', (user) => {
+      user.increments()
+      user.string('name', 255)
+      user.description('description')
+  })
 };
 
 exports.down = function(knex, Promise) {
