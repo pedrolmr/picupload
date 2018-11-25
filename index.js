07575025a2e03767cfafd9f3984022b853c00b73
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-25 02:56:33 -0500

    displaying posts' comments

--- index.js
+++ index.js
@@ -34,6 +34,22 @@ server.get('/api/users/:id', (req, res) => {
         .catch(err => res.status(500).json(err));
 });
 
+server.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    db('posts').where({ id }).first()
+        .then(post => {
+            if (post) {
+                db('comments')
+                    .where({ post_id: id })
+                    .then(comments => {
+                        post.comments = comments;
+                        res.status(200).json(post);
+                    })
+                    .catch(err => res.status(500).json(err))
+            }
+        })
+        .catch(err => res.status(500).json(err));
+});
 
 const port = 8000;
 server.listen(port, () => console.log(`running on port: ${port}`))
